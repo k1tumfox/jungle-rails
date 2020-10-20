ba1f20e1af3374484f41f4eba1879d268df4583c
Tenyson Kwon <tenysonkwon@gmail.com>
2020-10-20 15:46:59 +0000

    sold out integrated

--- db/seeds.rb
+++ db/seeds.rb
@@ -96,9 +96,10 @@ cat2.products.create!({
   name:  'Hotdog Slicer',
   description: Faker::Hipster.paragraph(4),
   image: open_asset('electronics2.jpg'),
-  quantity: 3,
+  quantity: 0,
   price: 26.00
 })
+# restore qty to 3
 
 cat2.products.create!({
   name:  'World\'s Largest Smartwatch',
