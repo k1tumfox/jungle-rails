195af7397418839fedb9036cb679f40b4f402527
Tenyson Kwon <tenysonkwon@gmail.com>
2020-11-19 17:51:43 -0500

    uses instance variables in controller to render product and category counts in erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,6 @@
 class ProductsController < ApplicationController
 
   def index
-    p ENV['STRIPE_PUBLISHABLE_KEY']
-    p ENV['ADMIN_USER_NAME']
     @products = Product.all.order(created_at: :desc)
   end
 
