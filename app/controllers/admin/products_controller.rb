be0dadad6138c99f310b65fae60e7bdb03e6c337
Tenyson Kwon <tenysonkwon@gmail.com>
2020-11-19 13:47:56 -0500

    fiddled with admin auth, may no be working due to WSL?

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['ADMIN_USER_NAME'], password: ENV['ADMIN_KEY']
+  http_basic_authenticate_with name: "Jungle", password: "book"
   
   def index
     @products = Product.order(id: :desc).all
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,8 @@
 class ProductsController < ApplicationController
 
   def index
+    p ENV['STRIPE_PUBLISHABLE_KEY']
+    p ENV['ADMIN_USER_NAME']
     @products = Product.all.order(created_at: :desc)
   end
 
